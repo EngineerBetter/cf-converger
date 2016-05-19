3ea3037ae00ee4ec5ab2620f1567b92f42956cbd
Daniel Jones <daniel.jones@engineerbetter.com>
2016-05-19 15:03:08 +0100

    add index handler

---
+++ src/main/java/com/engineerbetter/converger/http/IndexController.java
@@ -0,0 +1,13 @@
+package com.engineerbetter.converger.http;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Up";
+	}
+}
