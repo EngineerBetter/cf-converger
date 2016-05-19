8192ff336510d46af49953e3ca7182bf42469ac5
Daniel Jones <daniel.jones@engineerbetter.com>
2016-05-19 17:18:00 +0100

    upload and parse YAML as a tree

--- src/main/java/com/engineerbetter/converger/http/IndexController.java
+++ src/main/java/com/engineerbetter/converger/http/IndexController.java
@@ -1,10 +1,13 @@
 package com.engineerbetter.converger.http;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 @RestController
 public class IndexController {
@@ -14,8 +17,10 @@ public class IndexController {
 		return "Up";
 	}
 
-	@RequestMapping(method=RequestMethod.POST, value="/")
-	public String upload(@RequestParam("file") MultipartFile file) {
-		return "Size was: "+file.getSize();
+	@RequestMapping(method=RequestMethod.POST, value="/", consumes="application/yaml")
+	public String upload(@RequestBody byte[] declaration) throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+		JsonNode tree = objectMapper.readTree(declaration);
+		return "Converged org "+tree.get("org").get("name").asText();
 	}
 }
--- src/test/java/com/engineerbetter/converger/UploadTest.java
+++ src/test/java/com/engineerbetter/converger/UploadTest.java
@@ -3,13 +3,19 @@ package com.engineerbetter.converger;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.net.URI;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
@@ -17,14 +23,25 @@ import org.springframework.web.client.RestTemplate;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(ConvergerApplication.class)
 @WebIntegrationTest(randomPort=true)
-public class ConvergerSmokeTest {
+public class UploadTest {
 	@Value("${local.server.port}")
 	private int port;
 	private RestTemplate rest = new TestRestTemplate();
 
 	@Test
-	public void appRespondsOnRootPath()	{
-		ResponseEntity<String> response = rest.getForEntity("http://127.0.0.1:"+port, String.class);
+	public void triggersConvergence() throws Exception {
+		ClassPathResource fixture = new ClassPathResource("declaration.yml");
+		HttpHeaders requestHeaders = new HttpHeaders();
+		requestHeaders.add("Content-Type", "application/yaml");
+		requestHeaders.add("Accept", "application/json");
+
+		URI uri = new URI("http://127.0.0.1:"+port);
+		byte[] bytes = new byte[(int)fixture.contentLength()];
+		fixture.getInputStream().read(bytes);
+		RequestEntity<byte[]> postRequest = new RequestEntity<byte[]>(bytes, requestHeaders, HttpMethod.POST, uri);
+
+		ResponseEntity<String> response = rest.exchange(postRequest, String.class);
 		assertThat(response.getStatusCode(), is(HttpStatus.OK));
+		assertThat(response.getBody(), is("Converged org my-lovely-org"));
 	}
 }
