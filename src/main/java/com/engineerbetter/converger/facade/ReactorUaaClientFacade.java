9e768b25698739b9811d7a340f6e02d116b0a9f0
Daniel Jones <daniel.jones@engineerbetter.com>
2016-07-20 11:52:54 +0100

    UAA user lookup

--- src/main/java/com/engineerbetter/converger/facade/ReactorUaaClientFacade.java
+++ src/main/java/com/engineerbetter/converger/facade/ReactorUaaClientFacade.java
@@ -3,6 +3,8 @@ package com.engineerbetter.converger.facade;
 import java.util.Optional;
 
 import org.cloudfoundry.uaa.UaaClient;
+import org.cloudfoundry.uaa.users.ListUsersRequest;
+import org.cloudfoundry.uaa.users.ListUsersResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class ReactorUaaClientFacade implements UaaFacade
@@ -18,7 +20,14 @@ public class ReactorUaaClientFacade implements UaaFacade
 	@Override
 	public Optional<String> findUser(String username)
 	{
-		return null;
+		ListUsersResponse response = uaa.users().list(ListUsersRequest.builder().filter("username eq \""+username+"\"").build()).block();
+
+		if(response.getTotalResults() > 0)
+		{
+			return Optional.of(response.getResources().get(0).getId());
+		}
+
+		return Optional.empty();
 	}
 
 	@Override
---
+++ src/test/java/com/engineerbetter/converger/facade/ReactorUaaFacadeIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.engineerbetter.converger.facade;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.cloudfoundry.reactor.ConnectionContext;
+import org.cloudfoundry.reactor.DefaultConnectionContext;
+import org.cloudfoundry.reactor.TokenProvider;
+import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider;
+import org.cloudfoundry.reactor.uaa.ReactorUaaClient;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReactorUaaFacadeIntegrationTest
+{
+	private UaaFacade uaa;
+
+
+	@Before
+	public void setup()
+	{
+		String host = System.getenv("CF_HOST");
+		String username = System.getenv("CF_USERNAME");
+		String password = System.getenv("CF_PASSWORD");
+
+		if(host == null || username == null || password == null)
+		{
+			throw new RuntimeException("CF_HOST, CF_USERNAME and CF_PASSWORD must be set");
+		}
+
+		ConnectionContext connectionContext = DefaultConnectionContext.builder().apiHost(host).skipSslValidation(true).build();
+		TokenProvider tokenProvider = PasswordGrantTokenProvider.builder().username(username).password(password).build();
+
+		ReactorUaaClient uaaClient = ReactorUaaClient.builder().connectionContext(connectionContext).tokenProvider(tokenProvider).build();
+		uaa = new ReactorUaaClientFacade(uaaClient);
+	}
+
+
+	@Test
+	public void usersFindableByName()
+	{
+		Optional<String> id = uaa.findUser(System.getenv("CF_USERNAME"));
+		assertThat("User used to run tests should exist in the UAA", id.isPresent(), is(true));
+		assertThat(id.get(), not(""));
+	}
+}
